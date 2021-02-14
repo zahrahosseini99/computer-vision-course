b42111d933d59203fa4037a699aba3da7c0d8796
Zahra Hosseini <hosseini99.zahra@gmail.com>
2021-02-14 19:05:27 +0330

    Add hw8 files

---
+++ HW8/Q2.py
@@ -0,0 +1,19 @@
+def stitch(image1, image2):
+    '''
+    Creates panorama image of two inputs.
+    
+    Parameters:
+        image1 (numpy.ndarray): The first input image.
+        image2 (numpy.ndarray): The second input image.
+    
+    Returns:
+        numpy.ndarray: The result panorama image.
+    '''
+    
+    out_img = None
+    
+    #Write your code here
+
+    
+    return out_img
+
---
+++ HW8/Q3.py
@@ -0,0 +1,19 @@
+def put_mask(face, mask):
+    '''
+    Adds mask image on face image.
+    
+    Parameters:
+        face (numpy.ndarray): face image.
+        mask (numpy.ndarray): mask image.
+    
+    Returns:
+        numpy.ndarray: The result image.
+    '''
+    
+    result = face.copy()
+    
+    #Write your code here
+
+    
+    return result
+
